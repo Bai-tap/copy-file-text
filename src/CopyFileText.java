b300603883810b9a50f28e4fb9a782c0fcff9839
longhoang94 <longsutls@gmail.com>
2018-09-26 20:09:29 +0700

    XIN.
    Signed-off-by: longhoang94 <longsutls@gmail.com>

--- src/CopyFileText.java
+++ src/CopyFileText.java
@@ -4,17 +4,17 @@ public class CopyFileText {
     public static void main(String[] args) {
         try {
             CopyFileText resource = new CopyFileText();
+            System.out.println("Chau no truoc khi copy a.");
             String line = resource.readFileText("F:/resource.txt");
             File target = new File("F:/target.txt");
             FileWriter fw = new FileWriter(target);
             fw.write(line);
             fw.close();
-            System.out.println(fw);
+            System.out.println("Sau day la file text da duoc copy: ");
+            String readTarget = resource.readFileText("F:/target.txt");
         } catch (Exception e) {
-            System.err.println("WTF");
+            System.err.println(e.getMessage());
         }
-
-
     }
 
     public String readFileText(String filePath) throws Exception {
@@ -26,11 +26,13 @@ public class CopyFileText {
 
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line = "";
+        String s = "";
 
         while ((line = br.readLine()) != null) {
-            System.out.print(line + " ");
+            s += (line + " ");
         }
+        System.out.println(s);
         br.close();
-        return line;
+        return s;
     }
 }
